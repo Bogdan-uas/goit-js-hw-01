aaa0951edb052597dbe29210fd8f11b6695aa697
Bogdan_141 <bogdanmakovey1001@gmail.com>
2024-10-30 17:31:54 +0100

    final commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,12 @@
 function getElementWidth(content, padding, border) {
-    return(content + padding + padding + border + border)
+
+    const contentNumber = parseFloat(content);
+    const paddingNumber = parseFloat(padding);
+    const borderNumber = parseFloat(border);
+
+    return contentNumber + paddingNumber * 2 + borderNumber * 2;
 }
 
-console.log(getElementWidth(50, 8, 4)); // 74
-console.log(getElementWidth(60, 12, 8.5)); // 101
-console.log(getElementWidth(200, 0, 0)); // 200
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
